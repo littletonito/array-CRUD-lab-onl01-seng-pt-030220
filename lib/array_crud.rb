3b5d5cde3f1e7fd349ff3913fb8233c84fbd6152
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-03-11 16:20:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,11 +11,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array.unshift 
+  add_element_to_start_of_array.unshift("method")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  remove_element_from_end_of_array
 end
 
 def remove_element_from_start_of_array(array)
