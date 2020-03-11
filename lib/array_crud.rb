5d75ed5e7c1b84e139e059e4d94198814eba2827
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-03-11 16:19:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array << 5
+  add_element_to_end_of_array << "argument"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_element_to_start_of_array 
 end
 
 def remove_element_from_end_of_array(array)
