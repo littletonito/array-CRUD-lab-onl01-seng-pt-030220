7751b84e438abc3b74520688f71e3a875f6614e5
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-03-11 16:32:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array << 5
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
@@ -15,21 +15,21 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = remove_element_from_end_of_array.pop
+
 end
 
 def remove_element_from_start_of_array(array)
-  array = remove_element_from_start_of_array.shift
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_element_from_index[1]
+
 end
 
 def retrieve_first_element_from_array(array)
-  retrieve_first_element_from_array[0]
+
 end
 
 def retrieve_last_element_from_array(array)
-  retrieve_last_element_from_array[-1]
+
 end
